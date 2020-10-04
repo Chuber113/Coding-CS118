1bf0510e19e8515bd8adf01e718f2b6225df3cca
Chuber113 <70419345+Chuber113@users.noreply.github.com>
2020-10-03 23:04:16 -0700

    Add files via upload

---
+++ Concise code/Normal syntax.py
@@ -0,0 +1,16 @@
+import random
+
+lst = [2]
+
+for i in range(1, random.randint(5, 8)):
+    lst.append(random.randint(lst[i-1], (lst[i-1]*2)-1))
+
+if len(lst) % 2 == 0:
+    med = (lst[(int((len(lst)-1)/2))] + lst[(int((len(lst)-1)/2))+1])/2
+else:
+    med = lst[(int((len(lst)-1)/2))]
+
+#  I spent several hours on just trying to get the for loop to print in a fstring. I couldn't get it to work.
+for i in range(len(lst)):
+    print(lst[i], end=" ")
+print(f"Sum: {sum(lst)}, Median: {med}, Average: {sum(lst)/len(lst)}")
